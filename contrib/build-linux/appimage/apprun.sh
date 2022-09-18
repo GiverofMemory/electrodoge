2dfab3c4d4368ec2a040d9e4885933285c16aa29
GiverofMemory <78986433+GiverofMemory@users.noreply.github.com>
2022-09-18 16:59:32 -0400

    9-18-22 update

--- contrib/build-linux/appimage/apprun.sh
+++ contrib/build-linux/appimage/apprun.sh
@@ -7,9 +7,4 @@ APPDIR="$(dirname "$(readlink -e "$0")")"
 export LD_LIBRARY_PATH="${APPDIR}/usr/lib/:${APPDIR}/usr/lib/x86_64-linux-gnu${LD_LIBRARY_PATH+:$LD_LIBRARY_PATH}"
 export PATH="${APPDIR}/usr/bin:${PATH}"
 export LDFLAGS="-L${APPDIR}/usr/lib/x86_64-linux-gnu -L${APPDIR}/usr/lib"
-
-<<<<<<< HEAD
-exec "${APPDIR}/usr/bin/python3.9" -s "${APPDIR}/usr/bin/electrodoge" "$@"
-=======
-exec "${APPDIR}/usr/bin/python3" -s "${APPDIR}/usr/bin/electrum" "$@"
->>>>>>> 4f574afe5af0f169a7d2799e62b6052b472fc8ad
+exec "${APPDIR}/usr/bin/python3" -s "${APPDIR}/usr/bin/electrodoge" "$@"
